9869566543a7c20e50fc18fe1c1c59efb65b34e2
Denis MOUREU-LARRANG <dmoureu-@e1r12p2.42.fr>
2016-02-27 05:21:08 +0100

    79 leaks

--- tools.c
+++ tools.c
@@ -6,7 +6,7 @@
 /*   By: dmoureu- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/26 22:01:50 by dmoureu-          #+#    #+#             */
-/*   Updated: 2016/02/27 04:48:40 by dmoureu-         ###   ########.fr       */
+/*   Updated: 2016/02/27 05:19:49 by dmoureu-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 
 void	freestr(char *str)
 {
-	//if (*str)
-	//	free(str);
+	if (*str)
+		free(str);
 }
 
 void	freewstr(wchar_t *wstr)
 {
-//	if (*wstr && ft_wstrlen(wstr) > 0)
-//		free(wstr);
+	//if (*wstr && ft_wstrlen(wstr) > 0)
+	//	free(wstr);
 }
